5c8c03bfb83838b5de91825809c48724da682bb0
David Pernerstorfer <pernerstorfer.david@gmail.com>
2022-07-26 11:33:49 +0200

    add demo app for wide_alu test

---
+++ wide_alu/main.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include "pulp.h"
+
+int __rt_fpga_fc_frequency = 20000000;
+int __rt_fpga_periph_frequency = 10000000;
+
+int main()
+{
+    printf("Hello World!\n");
+
+    uint32_t a[32];
+    uint32_t b[32];
+    uint32_t expected_result[64];
+    uint32_t result[64];
+
+    memset(a, 0, sizeof (a));
+    memset(b, 0, sizeof (b));
+    memset(result, 0, sizeof (result));
+
+    a[0] = 113;
+    b[0] = 7;
+
+    set_delay(50);
+
+    // this operation will not actually multiply but instead perform an OR operation (optimized for synthesis)
+    wide_multiply(a, b, result);
+    expected_result[0] = a[0] | b[0];
+
+    printf("Expected Result: %d; Actual Result: %d\n", expected_result[0], result[0]);
+
+    return 0;
+}
